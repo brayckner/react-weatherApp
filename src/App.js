2b3f6409c4012f57e4f5ab13a9e9fd36225bea40
Brayckner <brayckner@gmail.com>
2021-10-13 23:36:16 -0400

    Add ability to query the API, Current have a 401 error saying that my API key is not activated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
 
 const api = {
-  key: "c1fd1efddd642b9340c2638f7f678a4b",
-  base: "http://api.openweathermap.org/data/2.5/"
-}
+  key: "5dc7407cf2f69d33e0b27a09d5b21af0  ",
+  base: "http://api.openweathermap.org/data/2.5/",
+};
+
+// Paused Youtube Video at 25:42
+// Got a 401 Error saying my API key was not activated
 
 function App() {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if(evt.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPTD=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(result);
+      });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    // let time = d.getTime();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
     <div className="App">
       <main>
         <div className="search-box">
           <input
-            type = "text"
-            className = "search-bar"
-            placeholder = "Search for a city or airport..."
-            />
+            type="text"
+            className="search-bar"
+            placeholder="Search for a city or airport..."
+
+            onChange={e => setQuery(e.target.value)}
+            value = {query}
+            onKeyPress = {search}
+          />
         </div>
+
         <div className="location-box">
-          <div className="location"></div>
-          <div className="date"></div>
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+
+        <div className="weather-box">
+          <div className="temp">68°F</div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
