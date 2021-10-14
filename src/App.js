1d09bf20252968bfc6866411a64cfef3fa8f1408
Brayckner <brayckner@gmail.com>
2021-10-13 22:18:46 -0400

    Feature 1: Added the Search bar, Got the OpenWeather Api key with base url, Added background images for both warm and cold temperatures

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+const api = {
+  key: "c1fd1efddd642b9340c2638f7f678a4b",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input
+            type = "text"
+            className = "search-bar"
+            placeholder = "Search for a city or airport..."
+            />
+        </div>
+        <div className="location-box">
+          <div className="location"></div>
+          <div className="date"></div>
+        </div>
+      </main>
     </div>
   );
 }
